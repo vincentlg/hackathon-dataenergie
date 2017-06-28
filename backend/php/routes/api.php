92bfc0a4a8c028421e960ead0d9e263ef34a9c2b
Vincent Le Gallic <legallic.vincent@gmail.com>
2017-06-28 23:14:57 +0200

    add first route

--- backend/php/routes/api.php
+++ backend/php/routes/api.php
@@ -12,7 +12,12 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+
+Route::group(['prefix' => 'v1', 'namespace' => 'Api\V1', 'middleware' => '\Barryvdh\Cors\HandleCors::class'], function () {
+    $this->get('users', 'UserController@index');
 });
